e40f95769465ac039fc8b8c96b9db606675234d2
Vincent Sanders <vincesanders@hotmail.com>
2020-06-03 10:53:55 -0500

    Got started on the markov cahllenge.

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -6,8 +6,22 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+wordsDict = {}
+startWords = []
+for i in range(len(words)):
+    if i == len(words) - 1:
+        break
+    if words[i] in wordsDict:
+        wordsDict[words[i]].append(words[i + 1])
+    else:
+        wordsDict[words[i]] = [words[i + 1]]
+        if words[i][0] == '"' or words[i[0]].isupper():
+            startWords.append(words[i])
 
 
 # TODO: construct 5 random sentences
 # Your code here
-
+current = random.choice(startWords)
+found_end = false
+sentence = ''
+while not found_end:
